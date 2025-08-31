65c219610ba967c6c33623ede8787f3abc31aea8
unknown <you@example.com>
2025-08-30 21:43:54 -0700

    call function added

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
 function getElement(id) {
-    const element = document.getElementById(id);
-    return element;
+  const element = document.getElementById(id);
+  return element;
 }
 
 const heartIcons = document.getElementsByClassName("flat-heart");
 for (const heartIcon of heartIcons) {
-    heartIcon.addEventListener("click", function () {
-        const heartCount = getElement("heart-count");
-        const totalHeartCount = Number(heartCount) + 1;
-        getElement("heart-count").innerText = totalHeartCount;
+  heartIcon.addEventListener("click", function () {
+    const heartCount = getElement("heart-count").innerText;
+    const totalHeartCount = Number(heartCount) + 1;
+    getElement("heart-count").innerText = totalHeartCount;
 
-    })
+  })
 }
+
+const callButtons = document.getElementsByClassName("call-btn");
+for (const callButton of callButtons) {
+  callButton.addEventListener("click", function () {
+    const card = callButton.closest('.card');
+    const title = card.querySelector('h2').innerText;
+    const subTitle = card.querySelectorAll('p')[0].innerText;
+    const number = card.querySelector('h1').innerText;
+
+    const coinCountElement = getElement('coin-count');
+    const coinCount = parseInt(coinCountElement.innerText)
+
+    if (coinCount >= 20) {
+      const TotalCoinCount = coinCount - 20;
+      coinCountElement.innerText = TotalCoinCount;
+      alert(` Calling ${subTitle} ${number}`)
+      const historyBoxContainer = getElement("history-box");
+      const historyBox = document.createElement("div");
+      historyBox.innerHTML = `<div
+          class="card-box flex flex-col sm:flex-row items-start sm:items-center justify-between p-4 gap-3 m-4 rounded-xl bg-[#FAFAFA]"
+        >
+          <div class="title">
+            <h1 class="text-sm md:text-base font-semibold">
+            ${title}
+            </h1>
+            <p class="text-gray-500 text-xs md:text-sm"><span>${number}</span></p>
+          </div>
+          <div class="time mt-2 sm:mt-0">
+            <h1 class="text-xs md:text-sm text-gray-600">${new Date().toLocaleTimeString()}</h1>
+          </div>
+        </div>`
+      historyBoxContainer.appendChild(historyBox);
+    } else {
+      alert("You've not sufficient coin.You need atleast 20 coin");
+    }
+  });
+}
+
